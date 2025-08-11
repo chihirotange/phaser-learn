9aabb7dbc52ff1cf3b4ed4a4bdb9d0311b6b2a75
Ai chan <ohanasakuiroha@gmail.com>
2025-08-12 00:05:34 +0700

    init

---
+++ src/scenes/Game.js
@@ -0,0 +1,64 @@
+import {Player} from '../gameObjects/Player.js';
+
+export class Game extends Phaser.Scene {
+    constructor() {
+        super('Game');
+    }
+
+    create() {
+        this.add.image(400, 300, 'sky')
+        this.platform = this.physics.add.staticGroup()
+        this.platform.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.platform.create(600, 400, 'ground');
+        this.platform.create(50, 250, 'ground');
+        this.platform.create(750, 220, 'ground');
+        this.player = new Player(this, 100, 450);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: {x: 12, y:0, stepX: 70}
+        })
+        this.stars.children.iterate(child =>
+            {
+                child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            });
+        this.physics.add.collider(this.player, this.platform);
+        this.physics.add.collider(this.stars, this.platform);
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+
+        this.score = 0;
+        this.scoreText = this.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#000'});
+    }
+
+    collectStar(player, star) {
+        star.disableBody(true, true);
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
+    }
+
+    update(time) {
+        if(this.cursors.left.isDown){
+            this.player.moveLeft();
+        }
+        else if (this.cursors.right.isDown){
+            this.player.moveRight();
+        }
+        else {
+            this.player.idle();
+        }
+
+        if (this.cursors.up.isDown){
+            this.player.jump();
+        }
+    }
+
+    moveSnake() {
+    }
+
+    spawnFood() {
+    }
+
+    gameOver() {
+    }
+}
